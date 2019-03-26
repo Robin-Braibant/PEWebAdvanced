ca2e0f8e15c94fe6f217692572a729b9c8a57181
MaximCoppieters <maximcoppieters@gmail.com>
2019-03-26 12:07:27 +0100

    Add connection factory

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php namespace Restaurant\data;
+
+include("/var/www/html/vendor/autoload.php");
+include("./src/container.php");
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App();
+
+$app->get('/', function(Request $request, Response $response) {
+    $response = $response->withHeader('Content-Type', 'text/html');
+
+    $response->getBody()->write('Hello world');
+
+    return $response;
+});
+
+$app->run();
