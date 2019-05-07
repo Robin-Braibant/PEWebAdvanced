f8d303cc420707e27e8cd22b9c8814c8a9425056
larsleppens <larsleppens@gmail.com>
2019-05-07 11:43:54 +0200

    removed unused js code

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,3 @@
-
-(function() {
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-    });
-})();
-
 function setMealToDelete(button) {
     const mealIdToDelete = button.id;
     $('#meal-id-input').val(mealIdToDelete);
